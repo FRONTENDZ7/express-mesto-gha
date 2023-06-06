0451ee90473223c9dcc5633a82bfe2b7d1865824
burondt <denisalexandrovich@inbox.ru>
2023-06-06 14:47:48 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,29 +1,29 @@
-const express = require("express");
-const mongoose = require("mongoose");
+const express = require('express');
+const mongoose = require('mongoose');
 
-const { PORT = 3000, BASE_PATH = "localhost" } = process.env;
-const { ERROR_NOT_FOUND } = require("./utils/response-status");
+const { PORT = 3000, BASE_PATH = 'localhost' } = process.env;
+const { ERROR_NOT_FOUND } = require('./utils/response-status');
 
 const app = express();
 
-const userRouter = require("./routes/users");
-const cardRouter = require("./routes/cards");
+const userRouter = require('./routes/users');
+const cardRouter = require('./routes/cards');
 
-const mongoDB = "mongodb://127.0.0.1:27017/mestodb";
-mongoose.set("strictQuery", false);
+const mongoDB = 'mongodb://127.0.0.1:27017/mestodb';
+mongoose.set('strictQuery', false);
 mongoose.connect(mongoDB);
 
 app.use(express.json());
 
 app.use((req, res, next) => {
-  req.user = { _id: "95k8gas9517j97x5fg8w1kv7" };
+  req.user = { _id: '95k8gas9517j97x5fg8w1kv7' };
   next();
 });
-app.use("/cards", cardRouter);
-app.use("/users", userRouter);
+app.use('/cards', cardRouter);
+app.use('/users', userRouter);
 
-app.use("*", (req, res) => {
-  res.status(ERROR_NOT_FOUND).send({ message: "Страница не найдена" });
+app.use('*', (req, res) => {
+  res.status(ERROR_NOT_FOUND).send({ message: 'Страница не найдена' });
 });
 
 app.listen(PORT, () => {
